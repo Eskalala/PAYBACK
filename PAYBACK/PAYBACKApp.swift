860ef29904ac6672b91ff1c07f3440fad8b35772
Jannis Klaus <jannisklaus.mgmail.com>
2024-10-22 16:06:52 +0200

    Initial Commit

---
+++ PAYBACK/PAYBACKApp.swift
@@ -0,0 +1,32 @@
+//
+//  PAYBACKApp.swift
+//  PAYBACK
+//
+//  Created by Jannis Klaus on 22.10.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PAYBACKApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
